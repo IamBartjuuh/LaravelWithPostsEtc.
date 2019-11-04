0032f9d229c0a8985de2e7fdf62a1518f177beef
Bart Atgier <contact@bartjuuh.nl>
2019-11-04 12:28:21 +0100

    Made function for open and closed status also made if thread is solved or unsolved. This is not functional right now.

---
+++ app/Enums/openclosed.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class openclosed extends Enum
+{
+    const Onopgelost = 0;
+    const Opgelost = 1;
+}
---
+++ app/Enums/status.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class status extends Enum
+{
+    const TicketOpen = 0;
+    const TicketClosed = 1;
+}
